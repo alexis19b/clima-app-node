a0045c7de9323e76ed3912f2c6967a8cd116b5cd
jesus <ingjesusalexis@gmail.com>
2021-08-17 09:16:16 -0400

    Version 0.1

--- models/busquedas.js
+++ models/busquedas.js
@@ -9,6 +9,13 @@ class Busquedas {
     //TODO: Leer Base de datos si exitos
     this.leerDB();
   }
+  get historialCapitalizado() {
+    return this.historial.map((lugar) => {
+      let palabras = lugar.split(" ");
+      palabras = palabras.map((p) => p[0].toUpperCase() + p.substring(1));
+      return palabras.join(" ");
+    });
+  }
   get paramsMaxbox() {
     return {
       access_token: process.env.MAPBOX_KEY,
@@ -65,6 +72,7 @@ class Busquedas {
     if (this.historial.includes(lugar.toLocaleLowerCase())) {
       return;
     }
+    this.historial = this.historial.splice(0, 5);
     this.historial.unshift(lugar.toLocaleLowerCase());
     this.guardarDB();
   }
@@ -75,6 +83,14 @@ class Busquedas {
     fs.writeFileSync(this.dbPath, JSON.stringify(payload));
   }
 
-  leerDB() {}
+  leerDB() {
+    if (!fs.existsSync(this.dbPath)) {
+      return null;
+    }
+    const info = fs.readFileSync(this.dbPath, { encoding: "utf-8" });
+    const data = JSON.parse(info);
+
+    this.historial = data.historial;
+  }
 }
 module.exports = Busquedas;
